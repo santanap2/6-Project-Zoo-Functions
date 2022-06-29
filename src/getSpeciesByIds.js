d47d34075ccf899865bc2db599fbc6a1f5446e2e
PedroSantana <phsantana99@gmail.com>
2022-06-29 17:23:22 -0300

    requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // iniciando projeto
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => data.species.find((animal) => animal.id === id));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
