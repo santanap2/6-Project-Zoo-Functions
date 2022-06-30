faecbbd02c20a0e5899f9dd1514d2baad2916cdb
PedroSantana <phsantana99@gmail.com>
2022-06-29 22:04:04 -0300

    requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
 function isManager(id) {
-  // seu código aqui
+  if (employees.find(() => id === stephanieId || id === olaId || id === burlId)) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const managerName = [];
+  employees.forEach((person) => {
+    if (person.managers.includes(managerId)) {
+      managerName.push(`${person.firstName} ${person.lastName}`);
+    }
+  });
+
+  return managerName;
 }
 
 module.exports = { isManager, getRelatedEmployees };
