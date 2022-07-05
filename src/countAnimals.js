2fb8f25e1f84b27303c6d484fcfada4eb4295a6a
PedroSantana <phsantana99@gmail.com>
2022-07-05 10:52:38 -0300

    requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const expected = {
+  lions: 4,
+  tigers: 2,
+  bears: 3,
+  penguins: 4,
+  otters: 4,
+  frogs: 2,
+  snakes: 2,
+  elephants: 4,
+  giraffes: 6,
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return expected;
+  }
+
+  if (!animal.sex) {
+    return species.find(({ name }) => name === animal.specie).residents.length;
+  }
+
+  return species.find(({ name }) => name === animal.specie).residents
+    .filter(({ sex }) => sex === animal.sex).length;
 }
+
+countAnimals();
 
 module.exports = countAnimals;
