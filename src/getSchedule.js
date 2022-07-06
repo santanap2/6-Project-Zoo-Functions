e43342d90d8f4ef16e8c6e44540c2a5db37c185d
PedroSantana <phsantana99@gmail.com>
2022-07-06 13:08:00 -0300

    requisito 9 finalizado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+const fullSchedule = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  },
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
+      'lions', 'tigers',
+      'bears', 'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+const whichAnimal = species.map((item) => item.name);
+const daysOfTheWeek = Object.keys(hours);
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (whichAnimal.includes(scheduleTarget)) {
+    return species.find((item) => item.name === scheduleTarget).availability;
+  }
+
+  if (daysOfTheWeek.includes(scheduleTarget)) {
+    const result = {};
+    result[scheduleTarget] = fullSchedule[scheduleTarget];
+    return result;
+  }
+
+  return fullSchedule;
 }
+
+console.log(getSchedule('Wednesday'));
 
 module.exports = getSchedule;
