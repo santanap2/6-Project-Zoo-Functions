f513a359c677ad9ad6e42de16ac70c2fb685b236
PedroSantana <phsantana99@gmail.com>
2022-07-08 22:53:21 -0300

    requisito 14 iniciado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const locationOrdened = () => {
+  const ne = [];
+  species.filter((one) => one.location === 'NE').forEach((item) => {
+    ne.push(item.name);
+  });
+  const nw = [];
+  species.filter((one) => one.location === 'NW').forEach((item) => {
+    nw.push(item.name);
+  });
+  const se = [];
+  species.filter((one) => one.location === 'SE').forEach((item) => {
+    se.push(item.name);
+  });
+  const sw = [];
+  species.filter((one) => one.location === 'SW').forEach((item) => {
+    sw.push(item.name);
+  });
+  const result = { NE: ne, NW: nw, SE: se, SW: sw };
+  return result;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) return locationOrdened();
 }
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
